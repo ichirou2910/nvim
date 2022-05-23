7cd07761b1f12d24a7bfae7fb6e708924b4a929e
Ichirou2910 <ichiroukeita2910@gmail.com>
2022-05-23 14:22:58 +0700

    split autocmds

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -50,6 +50,15 @@ vim.opt.cul = true -- cursor line
 
 vim.opt.timeoutlen = 500
 
+_G.gps_location = function()
+    local status, gps = pcall(require, "nvim-gps")
+    if not status then
+        return
+    end
+    return gps.is_available() and gps.get_location() or ""
+end
+vim.opt.winbar = "%{%v:lua.gps_location()%}"
+
 -- disable tilde on end of buffer: https://github.com/neovim/neovim/pull/8546#issuecomment-643643758
 vim.opt.fillchars = { eob = " " }
 
@@ -78,75 +87,3 @@ local disabled_built_ins = {
 for _, plugin in pairs(disabled_built_ins) do
     g["loaded_" .. plugin] = 1
 end
-
-local noAutoComment = vim.api.nvim_create_augroup("NoAutoComment", { clear = true })
-vim.api.nvim_create_autocmd("FileType", {
-    group = noAutoComment,
-    command = "setlocal formatoptions-=cro",
-    pattern = "*",
-})
-
--- Ibus settings
-vim.cmd([[
-    " Ibus settings
-    function! IBusOff()
-        " Save current engine
-        let g:ibus_prev_engine = system('ibus engine')
-        " Change to English engine
-        execute 'silent !ibus engine xkb:us::eng'
-    endfunction
-    function! IBusOn()
-        let l:current_engine = system('ibus engine')
-        " If engine was changed in Normal mode, use it instead
-        if l:current_engine !~? 'xkb:us::eng'
-          let g:ibus_prev_engine = l:current_engine
-        endif
-        " Restore engine
-        execute 'silent !' . 'ibus engine ' . g:ibus_prev_engine
-    endfunction
-
-    augroup IBusHandler
-        autocmd CmdLineEnter [/?] call IBusOn()
-        autocmd CmdLineLeave [/?] call IBusOff()
-        autocmd InsertEnter * call IBusOn()
-        autocmd InsertLeave * call IBusOff()
-    augroup END
-    call IBusOff()
-]])
-
--- Hide cursorline on inactive split
-local bgHighlight = vim.api.nvim_create_augroup("BgHighlight", { clear = true })
-vim.api.nvim_create_autocmd("WinEnter", {
-    group = bgHighlight,
-    command = "set cul",
-    pattern = "*",
-})
-vim.api.nvim_create_autocmd("WinLeave", {
-    group = bgHighlight,
-    command = "set nocul",
-    pattern = "*",
-})
-
--- Quickfix
-local quickfixForceBottom = vim.api.nvim_create_augroup("QuickfixForceBottom", { clear = true })
-vim.api.nvim_create_autocmd("FileType", {
-    group = quickfixForceBottom,
-    command = "wincmd J",
-    pattern = "qf",
-})
-
--- Highlight yank
-local highlightYank = vim.api.nvim_create_augroup("HighlightYank", { clear = true })
-vim.api.nvim_create_autocmd("TextYankPost", {
-    group = highlightYank,
-    command = "silent! lua vim.highlight.on_yank{higroup='IncSearch', timeout=700}",
-    pattern = "*",
-})
-
--- Project notes
-local projectNotes = vim.api.nvim_create_augroup("ProjectNotes", { clear = true })
-vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-    group = projectNotes,
-    command = "set filetype=notes | set syntax=markdown",
-    pattern = "*.notes",
-})
