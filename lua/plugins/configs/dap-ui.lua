17a430cdda22ac119e19c35a0c582a881436d088
Ichirou2910 <ichiroukeita2910@gmail.com>
2022-06-21 11:35:19 +0700

    dap-ui: migrate config

--- lua/plugins/configs/dap-ui.lua
+++ lua/plugins/configs/dap-ui.lua
@@ -7,23 +7,26 @@ require("dapui").setup({
         remove = "d",
         edit = "e",
     },
-    sidebar = {
-        elements = {
-            -- You can change the order of elements in the sidebar
-            "scopes",
-            "stacks",
-            "watches",
+    layouts = {
+        {
+            elements = {
+                "scopes",
+                "breakpoints",
+                "stacks",
+                "watches",
+            },
+            size = 60,
+            position = "left",
         },
-        size = 60,
-        position = "left", -- Can be "left" or "right"
+        {
+            elements = {
+                "repl",
+                "console",
+            },
+            size = 10,
+            position = "bottom",
+        },
     },
-
-    tray = {
-        elements = { "repl" },
-        size = 10,
-        position = "bottom", -- Can be "bottom" or "top"
-    },
-
     floating = {
         max_height = nil, -- These can be integers or a float between 0 and 1.
         max_width = nil, -- Floats will be treated as percentage of your screen.
