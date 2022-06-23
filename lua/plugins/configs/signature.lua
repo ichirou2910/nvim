875f0f7392b0b81a1009e99f7bb2232df060bb78
Ichirou2910 <ichiroukeita2910@gmail.com>
2022-06-23 17:43:44 +0700

    lsp signature

--- lua/plugins/configs/signature.lua
+++ lua/plugins/configs/signature.lua
@@ -1,16 +1,18 @@
-local present, lspsignature = pcall(require, "lsp_signature")
+local status_ok, signature = pcall(require, "lsp_signature")
 
-if present then
-
-    lspsignature.setup {
-        bind = true,
-        handler_opts = {border = "rounded"},
-        hint_enable = false,
-        hint_prefix = " ",
-        hint_scheme = "String",
-        floating_window_above_cur_line = true,
-        trigger_on_newline = false,
-        zindex = 50
-    }
-
+if not status_ok then
+    return
 end
+
+signature.setup({
+    bind = true,
+    handler_opts = { border = "rounded" },
+    max_width = 120,
+    hint_enable = true,
+    hint_prefix = " ",
+    hint_scheme = "String",
+    always_trigger = false,
+    floating_window_above_cur_line = true,
+    trigger_on_newline = false,
+    zindex = 50,
+})
