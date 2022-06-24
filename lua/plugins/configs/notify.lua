907b93af7acde013bf75f705dbf295440aa79520
Ichirou2910 <ichiroukeita2910@gmail.com>
2022-06-24 17:01:32 +0700

    notify: change style

--- lua/plugins/configs/notify.lua
+++ lua/plugins/configs/notify.lua
@@ -1,7 +1,7 @@
 require("notify").setup({
     -- Animation style
     -- fade_in_slide_out, slide, static, fade
-    stages = "slide",
+    stages = "fade_in_slide_out",
 
     -- Function called when a new window is opened, use for changing win settings/config
     on_open = nil,
