12fd8a9818568a0cab248dfb6b674d7e14611fba
Ichirou2910 <ichiroukeita2910@gmail.com>
2022-07-12 17:34:19 +0700

    csharp debug config

--- lua/plugins/configs/dap.lua
+++ lua/plugins/configs/dap.lua
@@ -68,8 +68,38 @@ dap.configurations.cs = {
         name = "launch - netcoredbg",
         request = "launch",
         preLaunchTask = "dotnet build",
+        cwd = function()
+            local cwd = vim.fn.getcwd()
+            local config_path = cwd .. "/.vim/dap.json"
+            local fd = vim.loop.fs_open(config_path, "r", 438)
+            if fd then
+                local stat = vim.loop.fs_fstat(fd)
+                if stat then
+                    local data = vim.loop.fs_read(fd, stat.size, 0)
+                    local workspace_path = vim.fn.json_decode(data)["cwd"]
+                    if workspace_path ~= nil then
+                        return cwd .. "/" .. workspace_path
+                    end
+                end
+            end
+
+            return cwd
+        end,
         program = function()
             local cwd = vim.fn.getcwd()
+            local config_path = cwd .. "/.vim/dap.json"
+            local fd = vim.loop.fs_open(config_path, "r", 438)
+            if fd then
+                local stat = vim.loop.fs_fstat(fd)
+                if stat then
+                    local data = vim.loop.fs_read(fd, stat.size, 0)
+                    local program_path = vim.fn.json_decode(data)["program"]
+                    if program_path ~= nil then
+                        return vim.fn.input("Path to dll: ", cwd .. "/" .. program_path, "file")
+                    end
+                end
+            end
+
             local d = vim.fn.fnamemodify(cwd, ":t")
             return vim.fn.input("Path to dll: ", cwd .. "/bin/Debug/netcoreapp3.1/" .. d .. ".dll", "file")
             -- return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
@@ -78,16 +108,16 @@ dap.configurations.cs = {
             ASPNETCORE_ENVIRONMENT = "Development",
         },
     },
-    -- {
-    --     type = "netcoredbg",
-    --     name = "attach - netcoredbg",
-    --     request = "attach",
-    --     processId = function()
-    --         local pid = require("dap.utils").pick_process()
-    --         vim.fn.setenv("NETCOREDBG_ATTACH_PID", pid)
-    --         return pid
-    --     end,
-    -- },
+    {
+        type = "netcoredbg",
+        name = "attach - netcoredbg",
+        request = "attach",
+        processId = function()
+            local pid = require("dap.utils").pick_process()
+            vim.fn.setenv("NETCOREDBG_ATTACH_PID", pid)
+            return pid
+        end,
+    },
 }
 
 dap.configurations.c = dap.configurations.cpp
