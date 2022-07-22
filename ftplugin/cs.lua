a6b23741291ab2c6c589098f5734092825ca15de
Ichirou2910 <ichiroukeita2910@gmail.com>
2022-07-22 15:12:10 +0700

    fix: csharp go-to-definition keymap

--- ftplugin/cs.lua
+++ ftplugin/cs.lua
@@ -1,1 +1,1 @@
-vim.keymap.set("n", "<space>gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<space>ld", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
