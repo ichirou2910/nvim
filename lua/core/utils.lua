76d27fdcf59e01eafe424fad18e0b59110d4c61b
Ichirou2910 <ichiroukeita2910@gmail.com>
2022-11-23 13:49:51 +0700

    chore: cleanup

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -119,47 +119,4 @@ M.isEmpty = function(src)
     return src == nil or src == ""
 end
 
-M.notify = {
-    spinner_frames = { "⣾", "⣽", "⣻", "⢿", "⡿", "⣟", "⣯", "⣷" },
-    client_notifs = {},
-    get_notif_data = function(client_id, token)
-        if not M.notify.client_notifs[client_id] then
-            M.notify.client_notifs[client_id] = {}
-        end
-
-        if not M.notify.client_notifs[client_id][token] then
-            M.notify.client_notifs[client_id][token] = {}
-        end
-
-        return M.notify.client_notifs[client_id][token]
-    end,
-
-    update_spinner = function(client_id, token)
-        local notif_data = M.notify.get_notif_data(client_id, token)
-
-        if notif_data.spinner then
-            local new_spinner = (notif_data.spinner + 1) % #M.notify.spinner_frames
-            notif_data.spinner = new_spinner
-
-            notif_data.notification = vim.notify(nil, nil, {
-                hide_from_history = true,
-                icon = M.notify.spinner_frames[new_spinner],
-                replace = notif_data.notification,
-            })
-
-            vim.defer_fn(function()
-                M.notify.update_spinner(client_id, token)
-            end, 100)
-        end
-    end,
-
-    format_title = function(title, client_name)
-        return client_name .. (#title > 0 and ": " .. title or "")
-    end,
-
-    format_message = function(message, percentage)
-        return (percentage and percentage .. "%\t" or "") .. (message or "")
-    end,
-}
-
 return M
