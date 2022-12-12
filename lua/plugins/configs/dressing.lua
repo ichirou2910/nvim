c8915668a56474eb9861ff2c8833a9a93d3e8040
Ichirou2910 <ichiroukeita2910@gmail.com>
2022-12-12 17:11:32 +0700

    plugin: bump config schema

--- lua/plugins/configs/dressing.lua
+++ lua/plugins/configs/dressing.lua
@@ -1,10 +1,12 @@
 require("dressing").setup({
     input = {
         relative = "cursor",
-        winblend = 10,
+        win_options = {
+            winblend = 10,
+            winhighlight = "NormalFloat:PreviewFloat,FloatBorder:PreviewBorder",
+        },
         insert_only = false,
         border = "rounded",
-        winhighlight = "NormalFloat:PreviewFloat,FloatBorder:PreviewBorder",
     },
     select = {
         backend = { "telescope", "fzf", "builtin" },
