000a88b7a74913ad400c2aedaadfb47820d8be31
Ichirou2910 <ichiroukeita2910@gmail.com>
2023-01-11 17:02:53 +0700

    cleanup

--- lua/plugins/configs/telescope.lua
+++ lua/plugins/configs/telescope.lua
@@ -84,6 +84,18 @@ require("telescope").setup({
             end,
         },
     },
+    extensions = {
+        fzf = {
+            fuzzy = true, -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true, -- override the file sorter
+            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+            -- the default case_mode is "smart_case"
+        },
+        file_browser = {
+            hijack_netrw = true,
+        },
+    },
     -- pickers = {
     --     buffers = {
     --         theme = "dropdown",
@@ -107,15 +119,6 @@ require("telescope").setup({
     --         },
     --     },
     -- },
-    extensions = {
-        fzf = {
-            fuzzy = true, -- false will only do exact matching
-            override_generic_sorter = true, -- override the generic sorter
-            override_file_sorter = true, -- override the file sorter
-            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-            -- the default case_mode is "smart_case"
-        },
-    },
 })
 
 require("telescope").load_extension("fzf")
