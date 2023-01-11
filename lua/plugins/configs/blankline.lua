82dd89dfaff3a930943a3ab22d23a0f8709e4b58
Ichirou2910 <ichiroukeita2910@gmail.com>
2023-01-11 16:54:00 +0700

    fix(indent): enable nolist

--- lua/plugins/configs/blankline.lua
+++ lua/plugins/configs/blankline.lua
@@ -4,16 +4,11 @@ vim.cmd([[
     endif
 ]])
 
-vim.cmd([[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]])
+vim.cmd([[highlight IndentBlanklineContextChar guifg=#C678DD gui=nocombine]])
+vim.cmd([[highlight IndentBlanklineContextStart guisp=#C678DD gui=underline]])
 
 require("indent_blankline").setup({
     viewport_buffer = 100,
-    indentLine_enabled = 1,
     char = "▏", -- '│'
     context_char = "▏", -- '│'
     filetype_exclude = {
@@ -29,12 +24,12 @@ require("indent_blankline").setup({
         "",
     },
     space_char_blankline = " ",
-    use_treesitter_scope = true,
+    use_treesitter = true,
     show_current_context = true,
+    show_current_context_start = true,
     show_trailing_blankline_indent = false,
     show_first_indent_level = false,
     show_foldtext = false,
     strict_tabs = true,
     max_indent_increase = 1,
-    disable_with_nolist = true,
 })
