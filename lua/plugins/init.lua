f297a230e4eacd751c4d9a24af2a5e4596620747
Ichirou2910 <ichiroukeita2910@gmail.com>
2023-02-21 14:48:55 +0700

    plugins: diffview

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -218,7 +218,11 @@ require("packer").startup(function(use)
     -- Git
     use({ "tpope/vim-fugitive", cmd = { "G", "Git" } })
     use("junegunn/gv.vim")
-    use({ "sindrets/diffview.nvim", requires = "nvim-lua/plenary.nvim" })
+    use({
+        "sindrets/diffview.nvim",
+        requires = "nvim-lua/plenary.nvim",
+        config = "require('plugins.configs.diffview')",
+    })
     use({
         "lewis6991/gitsigns.nvim",
         event = "BufReadPre",
