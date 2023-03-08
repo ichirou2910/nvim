f200ff8402c7cab81c207b22cdd9eacccb2b5f87
Ichirou2910 <ichiroukeita2910@gmail.com>
2023-03-08 09:16:25 +0700

    ui(treesitter): ensure installed all

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -1,6 +1,5 @@
 require("nvim-treesitter.configs").setup({
     ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-    ignore_install = { "godot_resource", "gdscript", "phpdoc" },
     highlight = {
         enable = true, -- false will disable the whole extension
         use_languagetree = true,
