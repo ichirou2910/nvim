7ce1a4af89924913db65f2cd86ae6c8e660188a0
Ichirou2910 <ichiroukeita2910@gmail.com>
2023-04-10 10:47:26 +0700

    treesitter: disable for big file

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -2,6 +2,13 @@ require("nvim-treesitter.configs").setup({
     ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
     highlight = {
         enable = true, -- false will disable the whole extension
+        disable = function(lang, buf)
+            local max_filesize = 1000 * 1024 -- 1 MB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+                return true
+            end
+        end,
         use_languagetree = true,
         additional_vim_regex_highlighting = false,
     },
