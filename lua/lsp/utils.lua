19d37adec469167b938317c2fe74bc4570f234b7
ichirou2910 <ichiroukeita2910@gmail.com>
2023-07-25 15:01:54 +0700

    fix(lsp): disable auto show diagnostics

--- lua/lsp/utils.lua
+++ lua/lsp/utils.lua
@@ -21,31 +21,38 @@ function M.lsp_diagnostics()
     })
 
     -- Show diagnostics in a pop-up window on hover
-    _G.LspDiagnosticsPopupHandler = function()
-        local current_cursor = vim.api.nvim_win_get_cursor(0)
-        local last_popup_cursor = vim.w.lsp_diagnostics_last_cursor or { nil, nil }
+    --[[ _G.LspDiagnosticsPopupHandler = function() ]]
+    --[[     local current_cursor = vim.api.nvim_win_get_cursor(0) ]]
+    --[[     local last_popup_cursor = vim.w.lsp_diagnostics_last_cursor or { nil, nil } ]]
+    --[[]]
+    --[[     -- Show the popup diagnostics window, ]]
+    --[[     -- but only once for the current cursor location (unless moved afterwards). ]]
+    --[[     if not (current_cursor[1] == last_popup_cursor[1] and current_cursor[2] == last_popup_cursor[2]) then ]]
+    --[[         vim.w.lsp_diagnostics_last_cursor = current_cursor ]]
+    --[[         vim.diagnostic.open_float({ ]]
+    --[[             scope = "cursor", ]]
+    --[[             focusable = false, ]]
+    --[[             border = border(), ]]
+    --[[         }) ]]
+    --[[     end ]]
+    --[[ end ]]
+    --[[]]
+    --[[ local lspHoverDiagnostic = vim.api.nvim_create_augroup("LspHoverDiagnostic", { clear = true }) ]]
+    --[[ vim.api.nvim_create_autocmd("CursorHold", { ]]
+    --[[     group = lspHoverDiagnostic, ]]
+    --[[     callback = function() ]]
+    --[[         _G.LspDiagnosticsPopupHandler() ]]
+    --[[     end, ]]
+    --[[     pattern = "*", ]]
+    --[[ }) ]]
 
-        -- Show the popup diagnostics window,
-        -- but only once for the current cursor location (unless moved afterwards).
-        if not (current_cursor[1] == last_popup_cursor[1] and current_cursor[2] == last_popup_cursor[2]) then
-            vim.w.lsp_diagnostics_last_cursor = current_cursor
-            vim.diagnostic.open_float({
-                scope = "cursor",
-                focusable = false,
-                border = border(),
-            })
-        end
-    end
-
-    local lspHoverDiagnostic = vim.api.nvim_create_augroup("LspHoverDiagnostic", { clear = true })
-    vim.api.nvim_create_autocmd("CursorHold", {
-        group = lspHoverDiagnostic,
-        callback = function()
-            _G.LspDiagnosticsPopupHandler()
-        end,
-        pattern = "*",
-    })
-
+    vim.keymap.set("n", "<leader>lk", function()
+        vim.diagnostic.open_float({
+            scope = "cursor",
+            focusable = false,
+            border = border(),
+        })
+    end, { noremap = true, silent = true })
     vim.keymap.set(
         "n",
         "gn",
