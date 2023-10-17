6e2a83c82a878c847193812b78b7d14aa6cfd914
ichirou2910 <ichiroukeita2910@gmail.com>
2023-10-17 14:08:53 +0700

    fix(plugins/blankline): migrate to v3

--- lua/plugins/configs/blankline.lua
+++ lua/plugins/configs/blankline.lua
@@ -1,59 +1,23 @@
-vim.cmd([[
-    if &diff
-        let g:indent_blankline_enabled = v:false
-    endif
-]])
-
-vim.cmd([[highlight IndentBlanklineContextStart1 guisp=#f7768e gui=underline]])
-vim.cmd([[highlight IndentBlanklineContextStart2 guisp=#e0af68 gui=underline]])
-vim.cmd([[highlight IndentBlanklineContextStart3 guisp=#9ece6a gui=underline]])
-vim.cmd([[highlight IndentBlanklineContextStart4 guisp=#1abc9c gui=underline]])
-vim.cmd([[highlight IndentBlanklineContextStart5 guisp=#7aa2f7 gui=underline]])
-vim.cmd([[highlight IndentBlanklineContextStart6 guisp=#bb9af7 gui=underline]])
-
-require("indent_blankline").setup({
-    viewport_buffer = 100,
-    char = "▏",
-    context_char = "▏",
-    filetype_exclude = {
-        "vimwiki",
-        "man",
-        "diagnosticpopup",
-        "lspinfo",
-        "packer",
-        "markdown",
-        "TelescopePrompt",
-        "TelescopeResults",
-        "WhichKey",
-        "",
-    },
-    context_highlight_list = {
-        "rainbowcol1",
-        "rainbowcol2",
-        "rainbowcol3",
-        "rainbowcol4",
-        "rainbowcol5",
-        "rainbowcol6",
-    },
-    context_start_highlight_list = {
-        "IndentBlanklineContextStart1",
-        "IndentBlanklineContextStart2",
-        "IndentBlanklineContextStart3",
-        "IndentBlanklineContextStart4",
-        "IndentBlanklineContextStart5",
-        "IndentBlanklineContextStart6",
-    },
-    space_char_blankline = " ",
-    use_treesitter = true,
-    show_current_context = true,
-    show_current_context_start = true,
-    show_trailing_blankline_indent = false,
-    show_first_indent_level = true,
-    show_foldtext = false,
-    strict_tabs = true,
-})
-
 local rainbow_delimiters = require("rainbow-delimiters")
+local hooks = require("ibl.hooks")
+
+local highlight = {
+    "TSRainbowRed",
+    "TSRainbowOrange",
+    "TSRainbowYellow",
+    "TSRainbowGreen",
+    "TSRainbowCyan",
+    "TSRainbowViolet",
+}
+
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+    vim.api.nvim_set_hl(0, "TSRainbowRed", { fg = "#ed8796" })
+    vim.api.nvim_set_hl(0, "TSRainbowOrange", { fg = "#f5a97f" })
+    vim.api.nvim_set_hl(0, "TSRainbowYellow", { fg = "#eed49f" })
+    vim.api.nvim_set_hl(0, "TSRainbowGreen", { fg = "#a6da95" })
+    vim.api.nvim_set_hl(0, "TSRainbowCyan", { fg = "#7dc4e4" })
+    vim.api.nvim_set_hl(0, "TSRainbowViolet", { fg = "#c6a0f6" })
+end)
 
 vim.g.rainbow_delimiters = {
     strategy = {
@@ -64,12 +28,26 @@ vim.g.rainbow_delimiters = {
         [""] = "rainbow-delimiters",
         lua = "rainbow-blocks",
     },
-    highlight = {
-        "rainbowcol1",
-        "rainbowcol2",
-        "rainbowcol3",
-        "rainbowcol4",
-        "rainbowcol5",
-        "rainbowcol6",
-    },
+    highlight = highlight,
 }
+
+require("ibl").setup({
+    indent = {
+        char = "▏",
+    },
+    scope = {
+        char = "▏",
+        highlight = highlight,
+    },
+    exclude = {
+        filetypes = {
+            "vimwiki",
+            "diagnosticpopup",
+            "packer",
+            "markdown",
+            "WhichKey",
+        },
+    },
+})
+
+hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
